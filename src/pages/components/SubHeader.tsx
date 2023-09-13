8867b7f7a11c2f46d7544441bfdb188c3ca70746
Gabriel <gkime91@gmail.com>
2023-09-12 17:43:22 -0700

    implemented typing effect.

--- src/pages/components/SubHeader.tsx
+++ src/pages/components/SubHeader.tsx
@@ -2,21 +2,35 @@ import React, {useEffect, useState} from 'react';
 
 export default function SubHeader({msg}) {
   const [ msgIndex, setMsgIndex ] = useState(0);
-  const [ text, setText ] = useState('');
+  const [ text, setText ] = useState(" ");
+  const [ bold, setBold ] = useState(false)
 
   useEffect(() => {
-    if (!msg[msgIndex]) return
+    if (!msg[msgIndex]) return;
+    const interval = msg[msgIndex - 1] === "." ? 500 : 55; // pause on end of sentence.
     const typeChars = setInterval(() => {
       setText(prevText => prevText += msg[msgIndex]);
       setMsgIndex(prevMsgIndex => prevMsgIndex += 1);
-    }, 100);
+    }, interval);
     return () => clearInterval(typeChars);
-  }, [])
+  }, [msgIndex])
+
+  useEffect(() => {
+    setBold(true)
+    console.log('true')
+  }, [msgIndex === msg.length])
 
   return (
-      <h1>
-        {text}
-      </h1>
-
+    <h1 className="h-10 font-mono text-center text-4xl ">
+          { 
+            msgIndex === msg.length && text.split(" ").map((str, index) => {
+              const key = str + index
+              if (!str.includes(".")) return <span key={key} className="font-extrabold">{str + " "}</span>
+              else return str + " ";
+            })
+            ||
+            text
+          }
+        </h1>
   )
 }
