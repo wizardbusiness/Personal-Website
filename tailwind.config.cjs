bbfca52b09f596d604351decd8de17cb75f5eb58
Gabriel <gkime91@gmail.com>
2023-09-13 11:40:59 -0700

    added blinking cursor anim utility.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      keyframes: {
+        blink: {
+          '50%': {opacity: '0'},
+          '100%': {opacity: '1'},
+        }
+      },
+      animation: {
+        'blinking-cursor': 'blink 1.5s steps(1) 6 forwards'
+      }
+    },
 	},
 	plugins: [],
 }
