d4c0f76149a4e7587c9bb602e4d1ade1152c1110
Gabriel <gkime91@gmail.com>
2023-09-23 12:37:43 -0700

    added accordion logic to techlist, using radix for headless

--- src/pages/components/TechList.tsx
+++ src/pages/components/TechList.tsx
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { forwardRef, type MutableRefObject } from "react";
+import * as Accordion from '@radix-ui/react-accordion';
 
+interface AccordionList {
+  items: string[];
+}
+
+const AccordionList = ({items}: AccordionList) => (
+  items.map((item, index) => 
+    <AccordionContent key={`${item}${index}`}>{item}</AccordionContent>)
+);
+  
 const TechList = ({ categories }) => {
   const list = categories.map((category: string, index: number) => (
-    <li key={category + index} className="text-2xl">
+    <Accordion.Root key={category + index} className="text-2xl" type="multiple">
+      <Accordion.Item value={category}>
+        <AccordionTrigger>{category}</AccordionTrigger>
+        <AccordionList items={['react', 'javascript', 'typescript']}></AccordionList>
+      </Accordion.Item>
       {" "}
-      + {category}
-    </li>
+    </Accordion.Root>
   ));
   return <ul>{list}</ul>;
 };
+
+interface AccordionI {
+  children: string,
+}
+
+const AccordionTrigger = forwardRef(({ children, ...props }: AccordionI, forwardedRef: MutableRefObject<HTMLButtonElement>) => (
+  <Accordion.Header>
+    <Accordion.Trigger 
+      {...props}
+      ref={forwardedRef}
+    >
+      +
+      {children}
+    </Accordion.Trigger>
+  </Accordion.Header>
+));
+
+
+const AccordionContent = forwardRef(({ children, ...props}: AccordionI, forwardRef: MutableRefObject<HTMLDivElement>) => (
+  <Accordion.Content
+    {...props}
+    ref={forwardRef}
+  >
+    <div>{children}</div>
+  </Accordion.Content>
+))
+
+
 
 export default TechList;
