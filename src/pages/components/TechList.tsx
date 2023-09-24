c7e61f4b10a76c30d09d56a92ad474d981aa1414
Gabriel <gkime91@gmail.com>
2023-09-23 22:07:41 -0700

    finally got accordion animations to work, thank the lord

--- src/pages/components/TechList.tsx
+++ src/pages/components/TechList.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, type MutableRefObject } from "react";
+import React, { useState, forwardRef, type MutableRefObject } from "react";
 import * as Accordion from "@radix-ui/react-accordion";
 
 const TechList = ({ categories }) => {
@@ -9,54 +9,22 @@ const TechList = ({ categories }) => {
         className="text-2xl"
         type="multiple"
       >
-        <Accordion.Item value={category}>
-          <AccordionTrigger>{category}</AccordionTrigger>
-          <AccordionList items={categories[category]}></AccordionList>
-        </Accordion.Item>{" "}
+        <AccordionItem categories={categories} category={category} />{" "}
       </Accordion.Root>
     ),
   );
   return <ul>{list}</ul>;
 };
 
-interface AccordionI {
-  lastItem?: boolean;
-  children: string;
-}
-
-const AccordionTrigger = forwardRef(
-  (
-    { children, ...props }: AccordionI,
-    forwardedRef: MutableRefObject<HTMLButtonElement>,
-  ) => (
-    <Accordion.Header>
-      <Accordion.Trigger {...props} ref={forwardedRef}>
-        +{children}
-      </Accordion.Trigger>
-    </Accordion.Header>
-  ),
-);
-
 interface AccordionList {
   items: string[];
+  open: boolean;
 }
 
-const AccordionList = ({ items }: AccordionList) =>
-  items.map((item, index) => {
+const AccordionedList = ({ items, open }: AccordionList) => {
+  const list = items.map((item, index) => {
     const lastItem = index === items.length - 1 ? true : false;
     return (
-      <AccordionContent lastItem={lastItem} key={`${item}${index}`}>
-        {item}
-      </AccordionContent>
-    );
-  });
-
-const AccordionContent = forwardRef(
-  (
-    { children, lastItem, ...props }: AccordionI,
-    forwardRef: MutableRefObject<HTMLDivElement>,
-  ) => (
-    <Accordion.Content {...props} ref={forwardRef}>
       <div
         className={`flex items-center border-l-2 border-gray-300 text-lg text-gray-600 ${
           lastItem
@@ -65,10 +33,67 @@ const AccordionContent = forwardRef(
         } translate-x-4`}
       >
         <div className="h-0.5 w-8 bg-gray-300" />
-        &nbsp;{children}
+        &nbsp;{item}
       </div>
-    </Accordion.Content>
+    );
+  });
+  return <AccordionContent>{list}</AccordionContent>;
+};
+
+const AccordionItem = ({ category, categories }) => {
+  const [open, setOpen] = useState(false);
+  const handleSetOpen = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
+  return (
+    <Accordion.Item className="overflow-hidden" value={category}>
+      <AccordionTrigger handleSetOpen={handleSetOpen}>
+        {category}
+      </AccordionTrigger>
+      <AccordionedList
+        open={open}
+        items={categories[category]}
+      ></AccordionedList>
+    </Accordion.Item>
+  );
+};
+
+interface AccordionI {
+  lastItem?: boolean;
+  handleSetOpen?: () => void;
+  children: string;
+  open?: boolean;
+}
+
+const AccordionTrigger = forwardRef(
+  (
+    { children, handleSetOpen, ...props }: AccordionI,
+    forwardedRef: MutableRefObject<HTMLButtonElement>,
+  ) => (
+    <Accordion.Header>
+      <Accordion.Trigger
+        onClick={() => handleSetOpen()}
+        {...props}
+        ref={forwardedRef}
+      >
+        +{children}
+      </Accordion.Trigger>
+    </Accordion.Header>
   ),
+);
+
+const AccordionContent = forwardRef(
+  ({ children, ...props }, forwardRef: MutableRefObject<HTMLDivElement>) => {
+    return (
+      <Accordion.Content
+        className="overflow:hidden radix-state-open:animate-open-accordion radix-state-closed:animate-close-accordion"
+        {...props}
+        ref={forwardRef}
+      >
+        {children}
+      </Accordion.Content>
+    );
+  },
 );
 
 export default TechList;
