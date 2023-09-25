07b113219b08742ae35e2e4f2779f6a3f7df7203
Gabriel <gkime91@gmail.com>
2023-09-25 12:07:53 -0700

    added keys to tech list categories.

--- src/pages/components/TechList.tsx
+++ src/pages/components/TechList.tsx
@@ -26,6 +26,7 @@ const AccordionedList = ({ items, open }: AccordionList) => {
     const lastItem = index === items.length - 1 ? true : false;
     return (
       <div
+        key={`item${index}`}
         className={`flex items-center border-l-2 border-gray-300 text-lg text-gray-600 ${
           lastItem
             ? "before:absolute before:z-10 before:h-4 before:w-1 before:-translate-x-1 before:translate-y-[56%] before:bg-white"
