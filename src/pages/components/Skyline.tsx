1046b88c68910662c55b5cbd2c6d4b963bb38e1b
Gabriel <gkime91@gmail.com>
2023-09-28 23:06:46 -0700

    implemented staggered transitions for city building effect.

--- src/pages/components/Skyline.tsx
+++ src/pages/components/Skyline.tsx
@@ -1,15 +1,23 @@
-import React, { useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
+import "../../styles/tailwind.css";
 
-const createSkylineBg = (numStructs: number) => {
+const createSkylineBg = (numStructs: number, direction: string) => {
   const structs = [];
-  while (numStructs > 0) {
-    structs.push(<SkylineShape key={`struct${numStructs}`} />);
-    numStructs--;
+  let i = 0;
+  while (i < numStructs) {
+    structs.push(
+      <SkylineShape delay={i} direction={direction} key={`struct${i}`} />,
+    );
+    i++;
   }
   return structs;
 };
 
-function SkylineShape() {
+function SkylineShape({ delay, direction }) {
+  const [addedClass, setAddedClass] = useState("");
+  useEffect(() => {
+    setAddedClass("build");
+  }, []);
   const randomIntFromInterval = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
@@ -25,26 +33,24 @@ function SkylineShape() {
   const shapeStyles = {
     height: height,
     width: width,
-    clipPath: `polygon(${pNodes[0]} 0%, ${pNodes[0]} 0, 100% ${pNodes[1]}, 100% 100%, 0 100%, 0 ${pNodes[2]})`,
+    clipPath: `polygon(100% 0, 100% 0, 100% 100%, 0 100%, 0 100)`,
+    transitionDelay:
+      direction === "right" ? `${300 - delay * 100}ms` : `${delay * 100}ms`,
   };
   return (
-    <div
-      data-effect
-      style={shapeStyles}
-      className="hidden animate-grow bg-gray-100"
-    />
+    <div data-effect style={shapeStyles} className={`structure bg-slate-100`} />
   );
 }
 
 const Skyline = () => {
-  const skylineLeft = useMemo(() => createSkylineBg(3), []);
-  const skylineRight = useMemo(() => createSkylineBg(3), []);
+  const skylineLeft = useMemo(() => createSkylineBg(3, "left"), []);
+  const skylineRight = useMemo(() => createSkylineBg(3, "right"), []);
   return (
     <div className="absolute h-32 w-1/2">
-      <div className="absolute bottom-0 left-2 flex items-end self-end">
+      <div className="absolute bottom-0 left-2 flex items-end gap-2 self-end">
         {skylineLeft}
       </div>
-      <div className="absolute bottom-0 right-2 flex items-end self-end">
+      <div className="absolute bottom-0 right-2 flex items-end gap-2 self-end">
         {skylineRight}
       </div>
     </div>
