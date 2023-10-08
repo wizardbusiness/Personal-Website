c43c7943bf754fb1925ee9c225ec7777781cba6d
Gabriel <gkime91@gmail.com>
2023-10-07 21:34:59 -0700

    broke subheader into two components to allow more control of state during transitions.

--- src/pages/components/SubHeader.tsx
+++ src/pages/components/SubHeader.tsx
@@ -1,41 +1,20 @@
 import React, { useEffect, useState, useMemo } from "react";
 import Cursor from "./Cursor";
 
-export default function SubHeader({ msg, blinkCursor }) {
-  const [msgIndex, setMsgIndex] = useState(0);
-  const [text, setText] = useState(" ");
-
-  useEffect(() => {
-    if (!msg[msgIndex]) return;
-    const interval = msg[msgIndex - 1] === "." ? 500 : 55; // pause on end of sentence.
-    const typeChars = setInterval(() => {
-      setText((prevText) => (prevText += msg[msgIndex]));
-      setMsgIndex((prevMsgIndex) => (prevMsgIndex += 1));
-    }, interval);
-    return () => clearInterval(typeChars);
-  }, [msgIndex]);
-
+export default function SubHeader({ repositTxt, children, animateBg }) {
   return (
     <div
       data-subheader
-      className="relative flex h-32 w-2/5 translate-x-1 items-center justify-center gap-2 whitespace-nowrap text-gray-100 before:h-32 before:w-full before:animate-squish-down before:rounded-md before:bg-foggy-glass"
+      className={`relative flex h-32 w-2/5 translate-x-1 items-center justify-center gap-2 whitespace-nowrap text-gray-100 before:h-32 before:w-full ${
+        animateBg && "before:animate-squish-down"
+      } before:rounded-md before:bg-foggy-glass`}
     >
-      <div className="absolute flex flex-nowrap gap-2">
-        <h1 className="no-wrap h-10 text-center font-mono text-4xl">
-          {(msgIndex === msg.length &&
-            text.split(" ").map((str, index) => {
-              const key = str + index;
-              if (!str.includes("."))
-                return (
-                  <span key={key} className="font-extrabold">
-                    {str + " "}
-                  </span>
-                );
-              else return str + " ";
-            })) ||
-            text}
-        </h1>
-        <Cursor animStart={blinkCursor && msgIndex === msg.length} />
+      <div
+        className={`absolute flex ${
+          repositTxt && "-translate-y-7"
+        } flex-nowrap gap-2`}
+      >
+        {children}
       </div>
     </div>
   );
---
+++ src/pages/components/SubheaderText.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import Cursor from "./Cursor";
+
+const SubheaderText = ({ msg, blinkCursor }) => {
+  const [text, setText] = useState(" ");
+  const [msgIndex, setMsgIndex] = useState(0);
+
+  useEffect(() => {
+    if (!msg[msgIndex]) return;
+    const interval = msg[msgIndex - 1] === "." ? 500 : 55; // pause on end of sentence.
+    const typeChars = setInterval(() => {
+      setText((prevText) => (prevText += msg[msgIndex]));
+      setMsgIndex((prevMsgIndex) => (prevMsgIndex += 1));
+    }, interval);
+    return () => clearInterval(typeChars);
+  }, [msgIndex]);
+
+  return (
+    <div className="flex gap-2">
+      <h1 className="no-wrap h-10 text-center font-mono text-4xl">
+        {(msgIndex === msg.length &&
+          text.split(" ").map((str, index) => {
+            const key = str + index;
+            if (!str.includes("."))
+              return (
+                <span key={key} className="font-extrabold">
+                  {str + " "}
+                </span>
+              );
+            else return str + " ";
+          })) ||
+          text}
+      </h1>
+      <Cursor animStart={blinkCursor && msgIndex === msg.length} />
+    </div>
+  );
+};
+
+export default SubheaderText;
