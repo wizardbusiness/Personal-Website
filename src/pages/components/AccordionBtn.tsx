db7feec2d26ac1e254d438c0f7277e9978d4d284
Gabriel <gkime91@gmail.com>
2023-10-13 13:55:47 -0700

    fiddling with smil animations

--- src/pages/components/AccordionBtn.tsx
+++ src/pages/components/AccordionBtn.tsx
@@ -11,17 +11,36 @@ const AccordionBtnIcon = ({ height, width, active }) => {
         y="40%"
         height="20%"
         width="100%"
-      />
-      {!active && (
-        <rect
-          id="vertical"
-          fill="#E5E7EB"
-          x="40%"
-          y="0"
-          height="100%"
-          width="20%"
+      >
+        <animateTransform begin="click" />
+      </rect>
+      <rect
+        id="vertical"
+        fill="#E5E7EB"
+        x="40%"
+        y="0"
+        height="100%"
+        width="20%"
+      >
+        <animateTransform
+          xlinkHref="#vertical"
+          begin="click"
+          attributeName="transform"
+          type="rotate"
+          dur="0.3s"
+          fill="freeze"
+          from={
+            active
+              ? `0 ${width / 2} ${height / 2}`
+              : `90 ${width / 2} ${height / 2}`
+          }
+          to={
+            active
+              ? `90 ${width / 2} ${height / 2}`
+              : `0 ${width / 2} ${height / 2}`
+          }
         />
-      )}
+      </rect>
     </svg>
   );
 };
