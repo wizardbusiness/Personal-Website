7316d9474925287496d693c5e9a07a5b8ee02fc7
Gabriel <gkime91@gmail.com>
2023-11-08 15:37:54 -0800

    added github actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({ config: { applyBaseStyles: false } })],
+  site: "https://github.com/wizardbusiness",
+  base: "/lunar-dev",
 });
