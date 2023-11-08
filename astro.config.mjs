d7f793fe51eaa504f7c371304944a67e199db09c
Gabriel <gkime91@gmail.com>
2023-11-08 15:44:03 -0800

    fixed site property for github deployment in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({ config: { applyBaseStyles: false } })],
-  site: "https://github.com/wizardbusiness",
+  site: "https://wizardbusiness.github.io",
   base: "/lunar-dev",
 });
