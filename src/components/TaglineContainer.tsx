97c48e91764da12fd4afcac66a1aecff95e71ed7
Gabriel <gkime91@gmail.com>
2023-11-08 17:07:06 -0800

    fixed mobile layout to account for device navbar

--- src/components/TaglineContainer.tsx
+++ src/components/TaglineContainer.tsx
@@ -6,7 +6,7 @@ export default function Tagline({ repositTxt, children, animateBg }) {
     <div
       data-subheader
       className={`relative flex items-center justify-center gap-2 whitespace-nowrap text-gray-100 
-        before:absolute before:bottom-1 before:z-auto before:h-24 before:w-[70vw] before:rounded-md 
+       before:bottom-1 before:z-auto before:h-24 before:w-[70vw] before:rounded-md 
         before:bg-foggy-glass sm:before:h-28 lg:h-32 lg:w-2/5 lg:before:w-[30vw] ${
           animateBg &&
           "lg:before:animate-squish-down-lg before:animate-squish-down-sm"
