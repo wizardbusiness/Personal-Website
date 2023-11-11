64d2ca8d2f9cefc3d5bccd5e4a1b39957229b0ca
Gabriel <gkime91@gmail.com>
2023-11-11 13:35:04 -0800

    removing some unused console .logs

--- src/components/TaglineContainer.tsx
+++ src/components/TaglineContainer.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useMemo } from "react";
 import Cursor from "./Cursor";
 
 export default function Tagline({ repositTxt, children, animateBg }) {
-  console.log(animateBg);
   return (
     <div
       data-subheader
@@ -10,7 +9,7 @@ export default function Tagline({ repositTxt, children, animateBg }) {
         before:z-auto before:h-24 before:w-[70vw] before:self-end before:rounded-md
         before:bg-foggy-glass before:sm:h-28 before:sm:w-[45vw] before:md:w-[35vw] lg:h-32 lg:before:w-[43vw] xl:before:w-[32vw] 2xl:before:w-[22vw] ${
           animateBg &&
-          "lg:before:animate-squish-down-lg before:animate-squish-down-sm"
+          "before:animate-squish-down-sm lg:before:animate-squish-down-lg"
         }`}
     >
       <div
