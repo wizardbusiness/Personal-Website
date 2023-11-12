a0f266a4e6677086adda1bbca904317fdfedd1b8
Gabriel <gkime91@gmail.com>
2023-11-12 12:16:47 -0800

    added in transform logic and am testing for safari mobile which seems to still be having issues registering touch events

--- src/components/PageScrollWidget.tsx
+++ src/components/PageScrollWidget.tsx
@@ -1,40 +1,75 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+} from "react";
 
 const PageScrollWidget = ({ pageUrl, scrollDirection, children }) => {
-  const scrollableAreaRef = useRef(null);
-  const scrollContainerRef = useRef(null);
-  const initialTouchPosit = useRef(null);
+  const [initialTouchPosit, setInitialTouchPosit] = useState(0);
+  const [currentTouchPosit, setCurrentTouchPosit] = useState(0);
+  const [transformDistanceY, setTransformDistanceY] = useState(0);
+  const [defaultCaretTopBound, setDefaultCaretTopBound] = useState(0);
+
+  let swipeableAreaRef = useRef(null);
+  const swipeContainerRef = useRef(null);
 
   useEffect(() => {
+    const handleSwipe = () => {};
     const taglineContainer = document.querySelector("[data-subheader]");
     const transitionGroup = document.querySelector("[data-transition-group]");
+    const transformDistanceY = currentTouchPosit - initialTouchPosit;
+
+    if (
+      swipeContainerRef.current?.getBoundingClientRect().top -
+        defaultCaretTopBound <
+      transformDistanceY
+    ) {
+      setTransformDistanceY(transformDistanceY);
+    }
+  }, [currentTouchPosit]);
+
+  useEffect(() => {
+    setDefaultCaretTopBound(
+      swipeableAreaRef.current?.getBoundingClientRect().top,
+    );
   }, []);
 
+  const handleTouchStart = (e) => {
+    setInitialTouchPosit(e.touches[0].clientY);
+    // swipeableAreaRef.current = {
+    //   clientY: e.touches[0].clientY,
+    //   top: swipeContainerTopPosit,
+    // };
+  };
+
+  const handleTouchMove = (e) => {
+    setCurrentTouchPosit(e.touches[0].clientY);
+  };
+
   const handleSwipe = () => {
-    let initialTouchPosit: number;
-    let touchClientY: number;
-    let swipeComplete: boolean;
-    const handleTouchMove = (e: TouchEvent) => {
-      // if (swipeComplete) {
-      //   scrollContainer.removeEventListener("touchmove", handleTouchMove);
-      //   scrollableArea.classList.add("animate-fade-out");
-      //   transitionGroup.classList.add("transition");
-      //   taglineContainer.classList.add("animate-fall-from");
-      //   taglineContainer.addEventListener("animationend", () => {
-      //     scrollCaret.classList.remove("move-down");
-      //     transitionGroup.classList.remove("transition");
-      //     scrollCaret.click();
-      //   });
-      // }
-    };
+    // if (swipeComplete) {
+    //   scrollContainer.removeEventListener("touchmove", handleTouchMove);
+    //   scrollableArea.classList.add("animate-fade-out");
+    //   transitionGroup.classList.add("transition");
+    //   taglineContainer.classList.add("animate-fall-from");
+    //   taglineContainer.addEventListener("animationend", () => {
+    //     scrollCaret.classList.remove("move-down");
+    //     transitionGroup.classList.remove("transition");
+    //     scrollCaret.click();
+    //   });
+    // }
   };
   return (
     <div
-      ref={scrollContainerRef}
-      onTouchStart={(e) => (initialTouchPosit.current = e.touches[0].clientY)}
+      ref={swipeContainerRef}
       className="flex h-1/5 w-full items-end justify-center"
     >
       <a
+        onTouchMove={handleTouchMove}
+        onTouchStart={handleTouchStart}
+        style={{
+          transform: `translateY(${transformDistanceY}px)`,
+        }}
         href={pageUrl}
         data-scroll-btn={scrollDirection}
         className={
@@ -42,14 +77,14 @@ const PageScrollWidget = ({ pageUrl, scrollDirection, children }) => {
             ? "nav-scroll m-0 flex w-[40vw] translate-y-1/2 cursor-default flex-col justify-center text-lg lg:h-[10vh] lg:-translate-y-4"
             : "flex w-[50vw] flex-col items-center lg:translate-y-4"
         }
-        ref={scrollableAreaRef}
+        ref={swipeableAreaRef}
       >
         {scrollDirection === "down" && (
           <div
             data-test
             className=" w-fit translate-y-2 text-center text-gray-400 "
           >
-            Scroll
+            {transformDistanceY}
           </div>
         )}
         {children}
