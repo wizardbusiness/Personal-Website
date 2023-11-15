01023163652df82c00e170ed8d7e49a823ae3204
Gabriel <gkime91@gmail.com>
2023-11-14 16:16:15 -0800

    added animations for transitioning to prev page

--- src/components/TaglineContainer.tsx
+++ src/components/TaglineContainer.tsx
@@ -9,7 +9,7 @@ export default function Tagline({ repositTxt, children, animateBg }) {
         before:z-auto before:h-24 before:w-[70vw] before:self-end before:rounded-md
         before:bg-foggy-glass before:sm:h-28 before:sm:w-[45vw] before:md:w-[35vw] lg:h-32 lg:before:w-[43vw] xl:before:w-[32vw] 2xl:before:w-[22vw] ${
           animateBg &&
-          "before:animate-squish-down-sm lg:before:animate-squish-down-lg"
+          "before:animate-squish-down-sm before:[transform-origin:bottom] lg:before:w-[70vw] lg:before:scale-y-[4%] lg:before:animate-squish-down-lg"
         }`}
     >
       <div
