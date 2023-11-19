4c0afdc2b9f180a57e7fcf9510b50d8dd2fcf16d
Gabriel <gkime91@gmail.com>
2023-11-19 13:45:27 -0800

    updated animationend of component to reset localstorage property to prevent nav prev navigation on initial navigation or page refresh on landing

--- src/components/TaglineContainer.tsx
+++ src/components/TaglineContainer.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useState, useMemo } from "react";
-import Cursor from "./Cursor";
+import React from "react";
 
 export default function Tagline({ repositTxt, children, animateBg }) {
   return (
     <div
       data-subheader
-      className={`relative flex items-center justify-center gap-2 whitespace-nowrap
-        before:z-auto before:h-24 before:w-[70vw] before:self-end before:rounded-md
-        before:bg-foggy-glass before:sm:h-28 before:sm:w-[45vw] before:md:w-[35vw] lg:h-32 lg:before:w-[43vw] xl:before:w-[32vw] 2xl:before:w-[22vw] ${
+      className={`relative flex items-center justify-center gap-2 whitespace-nowrap before:z-auto 
+        before:h-24 before:w-[70vw] before:self-end
+        before:rounded-md before:bg-foggy-glass before:sm:h-28 before:sm:w-[45vw] before:md:w-[35vw] lg:h-32 lg:before:w-[43vw] xl:before:w-[32vw]
+        2xl:before:w-[22vw] ${
           animateBg &&
           "before:animate-squish-down-sm before:[transform-origin:bottom] lg:before:w-[70vw] lg:before:scale-y-[4%] lg:before:animate-squish-down-lg"
         }`}
+      onAnimationEnd={() => {
+        if (!location.href.includes("/Main")) {
+          localStorage.setItem("navPrev", JSON.stringify(false));
+        }
+      }}
     >
       <div
         className={`absolute flex ${
