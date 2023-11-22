636e8148d5d892fcd09bf5f65e0c6569429af301
Gabriel <gkime91@gmail.com>
2023-11-22 13:18:01 -0800

    added function and type to choose random element from array

--- src/scripts/randomFromInterval.ts
+++ src/scripts/randomFromInterval.ts
@@ -1,3 +1,5 @@
+import type { ReactElement, FC } from "react";
+
 export const randomIntFromInterval = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
@@ -11,4 +13,11 @@ export const randomDecNumFromInterval = (
     Math.round((Math.random() * (max - min) + min) * 10 ** decPlaces) /
     10 ** decPlaces
   );
+};
+
+type elArray = (number | string)[] | FC[];
+export const randomElFromArray = (elArray: elArray): number | string | FC => {
+  const elsLength = elArray.length;
+  const randomIndex = Math.floor(Math.random() * elsLength);
+  return elArray[randomIndex];
 };
