60716affac6f28cde0e9c32bafe4f965a63575ce
Gabriel <gkime91@gmail.com>
2023-11-30 15:52:33 -0800

    created function to handle normal dist generation

--- src/scripts/randomFromInterval.ts
+++ src/scripts/randomFromInterval.ts
@@ -21,3 +21,52 @@ export const randomElFromArray = (elArray: elArray): number | string | FC => {
   const randomIndex = Math.floor(Math.random() * elsLength);
   return elArray[randomIndex];
 };
+
+export const generateWeightedRandomInRange = (
+  min: number,
+  max: number,
+  mean: number,
+  variance: number,
+) => {
+  const u1 = Math.random();
+  const u2 = Math.random();
+
+  const weightedRandom =
+    mean + Math.sqrt(-2 * Math.log(u1) * Math.cos(2 * Math.PI * u2) * variance);
+
+  return Math.max(min, Math.min(max, weightedRandom));
+};
+
+/**
+ *
+ * @param min
+ * @param max
+ * @param curve
+ *
+ */
+
+type coordNode = {
+  x: number;
+  y: number;
+};
+export const generatedWeightedCoords = (
+  innerWidth: number,
+  innerHeight: number,
+  variance: number,
+  slope: number,
+  intercept: number,
+  i: number,
+  count: number,
+): coordNode => {
+  const normalRandom = () => {
+    return (
+      Math.sqrt(-2 * Math.log(Math.random())) *
+      Math.cos(2 * Math.PI * Math.random())
+    );
+  };
+
+  const x = (innerWidth / count) * i;
+  const y = i * slope + normalRandom() * variance + intercept;
+
+  return { x: x, y: y };
+};
