60ab8ed6792841f17f39a0517cea3d9a8f9a4a25
Gabriel <gkime91@gmail.com>
2024-01-28 22:16:46 -0800

    removed unnecessary positioning

--- src/components/TaglineContainer.tsx
+++ src/components/TaglineContainer.tsx
@@ -4,7 +4,7 @@ export default function Tagline({ repositTxt, children, animateBg }) {
   return (
     <div
       data-subheader
-      className={`relative flex items-center justify-center gap-2 whitespace-nowrap before:z-auto 
+      className={`flex items-center justify-center gap-2 whitespace-nowrap before:z-auto 
         before:h-24 before:w-[70vw] before:self-end
         before:rounded-md before:bg-foggy-glass before:sm:h-28 before:sm:w-[45vw] before:md:w-[35vw] lg:h-32 lg:before:w-[43vw] xl:before:w-[32vw]
         2xl:before:w-[22vw] ${
