7f46b142ce693be5034d169155808e5eff2a6d69
Gabriel <gkime91@gmail.com>
2024-02-08 20:03:51 -0800

    fixed responsive layout scaling

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cursor = ({ animStart }) => {
   return (
     <div
-      className={`inline-block h-6 w-3 bg-gray-100 lg:h-8 lg:w-4 ${
+      className={`inline-block h-6 w-3 bg-gray-100 lg:h-8 lg:w-4 xl:h-12 xl:w-7 2xl:h-20 2xl:w-10 ${
         animStart && "animate-blinking-cursor"
       }`}
       onAnimationEnd={(e) => e.stopPropagation()}
