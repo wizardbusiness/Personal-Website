faa83160706abd4c175183d96231c25fa53754e3
Gabriel <gkime91@gmail.com>
2024-02-12 20:33:40 -0800

    Added conditional scaling styling to component- used when is child of  about section CaptionContainer component

--- src/components/TaglineMsg.tsx
+++ src/components/TaglineMsg.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Cursor from "./Cursor";
 
-const TaglineMsg = ({ msg, blinkCursor }) => {
+const TaglineMsg = ({ msg, blinkCursor, scaleText }) => {
   const [text, setText] = useState(" ");
   const [msgIndex, setMsgIndex] = useState(0);
 
@@ -16,7 +16,11 @@ const TaglineMsg = ({ msg, blinkCursor }) => {
   }, [msgIndex]);
 
   return (
-    <div className="flex items-center gap-2">
+    <div
+      className={`flex items-center gap-2 ${
+        scaleText && "scale-[130%]"
+      } [transform-origin:bottom]`}
+    >
       <h1 className="no-wrap h-fit text-center font-mono text-xl text-gray-200 lg:text-3xl xl:text-5xl 2xl:text-7xl">
         {(msgIndex === msg.length &&
           text.split(" ").map((str, index) => {
