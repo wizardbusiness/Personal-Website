86bc3f4a6556ca2c39b6669b03b06c705a843a6a
Gabriel <gkime91@gmail.com>
2024-03-17 22:46:17 -0700

    added functions for scroll, checking if caption container at certain position in window,  animations, translation, opacity

--- src/scripts/transition.ts
+++ src/scripts/transition.ts
@@ -19,8 +19,11 @@ const aboutSectionPreNavArea: HTMLElement = document.querySelector("#nav-check")
 
 // data-attributes
 
-// functions
+// ------------------------------------------------------------------------------------------------------
+// FUNCTIONS
+// ------------------------------------------------------------------------------------------------------
 
+// ******************************************************************************************************
 // Move captionContainer
 function moveCaptionComponent(
   captionComponent: HTMLElement,
@@ -49,3 +52,133 @@ function moveCaptionComponentUp() {
   let destination = landingComponentInitPosit.top;
   moveCaptionComponent(captionComponentAbout, 15, 1.02, 20, -1, destination);
 }
+
+// ******************************************************************************************************
+
+function checkIfCaptionComponentAtPosition(
+  currComponentPosition: number,
+  targetPosition: number,
+  callback: Function,
+  cbArgs: HTMLElement[],
+) {
+  if (currComponentPosition === targetPosition) {
+    callback(cbArgs);
+  } else {
+    requestAnimationFrame(() => {
+      checkIfCaptionComponentAtPosition(currComponentPosition, targetPosition, callback, cbArgs);
+    });
+  }
+}
+
+function checkIfLandingCaptionAtCoords() {
+  const currComponentPosition = document
+    .querySelectorAll("#caption-container")[0]
+    .getBoundingClientRect().top;
+  const targetPosition = window.innerHeight * 0.55;
+  const cbArgs: HTMLElement[] = [aboutSectionContentGroup];
+  const handleAtTargetPosition = (cbArgs: HTMLElement[]) => {
+    cbArgs.forEach((element) => element.classList.replace("translate-y-[100vh]", "translate-y-[0vh]"));
+  };
+  checkIfCaptionComponentAtPosition(currComponentPosition, targetPosition, handleAtTargetPosition, cbArgs);
+}
+
+function checkIfAboutCaptionAtCoords() {
+  const currComponentPosition = document
+    .querySelectorAll("#caption-container")[1]
+    .getBoundingClientRect().top;
+  const targetPosition = landingComponentInitPosit.top;
+  const cbArgs: HTMLElement[] = [...landingSectionContentGroup];
+  const handleAtTargetPosition = (cbArgs: HTMLElement[]) => {
+    cbArgs.forEach((element) => {
+      element.classList.replace("animate-slide-up", "animate-slide-down");
+      element.classList.replace("opacity-0", "opacity-1");
+    });
+  };
+  checkIfCaptionComponentAtPosition(currComponentPosition, targetPosition, handleAtTargetPosition, cbArgs);
+}
+
+// ******************************************************************************************************
+
+function programaticallyScrollToNextSection() {
+  window.scrollBy({
+    // landing section height + nav check div height
+    top: window.innerHeight,
+    behavior: "smooth",
+  });
+}
+
+// ******************************************************************************************************
+
+// all animations that can apply to an element involved in the transition
+// NOTE: not exhaustive to all animations used in project
+
+const animations = [
+  "animate-slide-up",
+  "animate-slide-down",
+  "animate-scale-up",
+  "animate-squish-down-lg",
+  "animate-squish-down-sm",
+  "before:animate-squish-down-lg",
+  "animate-squelch",
+  "before:animate-squelch",
+  "animate-float-up",
+  "animate-grow",
+  "animate-fade-in",
+  "animate-fade-out",
+  "fade-out-slow",
+  "scooch-up",
+  "long-blink",
+] as const;
+
+type Animations = (typeof animations)[number];
+
+function animateElements(animation: Animations) {
+  // const animations: Animations[] = Object.values(Animations);
+  return (elements: NodeListOf<HTMLElement>) => {
+    // remove all animations from the element before
+    elements.forEach((element) => {
+      animations.forEach((animation) => {
+        if (element.classList.contains(animation)) {
+          element.classList.remove(animation);
+        }
+      });
+    });
+    elements.forEach((element) => {
+      element.classList.add(animation);
+    });
+  };
+}
+
+const slideUpAndFade = animateElements("animate-slide-up");
+const slideDown = animateElements("animate-slide-down");
+
+// ******************************************************************************************************
+const translations = ["translate-y-[0vh]", "translate-y-[100vh]"] as const;
+
+type Translations = (typeof translations)[number];
+
+function translateElements(freshTranslation: Translations, staleTranslation: Translations) {
+  return (elements: HTMLElement[]) => {
+    elements.forEach((element) => {
+      element.classList.replace(staleTranslation, freshTranslation);
+    });
+  };
+}
+
+const translateSectionContentUp = translateElements("translate-y-[100vh]", "translate-y-[0vh]");
+const translateSectionContentDown = translateElements("translate-y-[0vh]", "translate-y-[100vh]");
+// ------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------
+
+const opacities = ["opacity-[0.8]", "opacity-0"] as const;
+
+type Opacities = (typeof opacities)[number];
+
+function changeElementOpacity(freshOpacity: Opacities, staleOpacity: Opacities) {
+  return (element: HTMLElement) => {
+    element.classList.replace(staleOpacity, freshOpacity);
+  };
+}
+
+const increaseElementOpacity = changeElementOpacity("opacity-0", "opacity-[0.8]");
+const decreaseElementOpacity = changeElementOpacity("opacity-[0.8]", "opacity-0");
