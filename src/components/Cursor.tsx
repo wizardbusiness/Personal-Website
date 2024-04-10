656c82c0709f987f2e298a1c231d39405a7d0fd8
Gabriel <gkime91@gmail.com>
2024-04-09 22:19:40 -0700

    adjusted cursor and font size to be larger

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Cursor = ({ animStart }) => {
   return (
     <div
-      className={`inline-block h-6 w-3 bg-gray-100 lg:h-8 lg:w-4 xl:h-12 xl:w-7 2xl:h-20 2xl:w-10 ${
+      className={`inline-block h-7 w-4 bg-gray-100 lg:h-9 lg:w-5 xl:h-12 xl:w-7 2xl:h-20 2xl:w-10 ${
         animStart && "animate-blinking-cursor"
       }`}
       onAnimationStart={(e) => e.stopPropagation()}
