c97547809cd7ccaba9f8d8d10b0ebf1ec35d14bb
Gabriel <gkime91@gmail.com>
2024-04-30 12:21:58 -0700

    fixed type errors for randomElFromArray function

--- src/scripts/randomFromInterval.ts
+++ src/scripts/randomFromInterval.ts
@@ -8,8 +8,8 @@ export const randomDecNumFromInterval = (min: number, max: number, decPlaces = 1
   return Math.round((Math.random() * (max - min) + min) * 10 ** decPlaces) / 10 ** decPlaces;
 };
 
-type elArray = (number | string)[];
-export const randomElFromArray = (elArray: elArray): number | string => {
+type elArray = (number | string)[] | FC[];
+export const randomElFromArray = (elArray: elArray): (number | string) | FC => {
   const elsLength = elArray.length;
   const randomIndex = Math.floor(Math.random() * elsLength);
   return elArray[randomIndex];
