bebbec457bf4f383ee7d63f974da7be5e37ffc54
Gabriel <gkime91@gmail.com>
2024-05-01 22:32:10 -0700

    added mutation observer hook in island root component to check if css variable in document root which controls component width is loaded yet. made component width dynamic using css variable. removed logic causing unnecessary rerenders.

--- src/components/bgEffects/Skyline.tsx
+++ src/components/bgEffects/Skyline.tsx
@@ -1,92 +1,9 @@
 import React, { useState, useMemo, useEffect, useRef, type Dispatch, type SetStateAction } from "react";
+import useRootMutationObserver from "../../scripts/hooks/useMutationObserver";
 import { randomIntFromInterval } from "../../scripts/randomFromInterval";
 import Forest from "./Forest";
 import CityPark from "./CityPark";
 import "../../styles/tailwind.css";
-
-const Building = ({ delay, height, width }) => {
-  const [build, setBuild] = useState(false);
-  const [render, setRender] = useState(false);
-  const [, setTransitionEnd] = useState(false);
-  const shapeRef = useRef(null);
-
-  const handleTransitionEnd = () => {
-    setTransitionEnd(true);
-  };
-
-  useEffect(() => {
-    setRender(true);
-
-    const delayBuild = setTimeout(() => {
-      setBuild(true);
-    }, 375);
-    return () => {
-      clearTimeout(delayBuild);
-    };
-  }, [render]);
-  // only represent values for targetable nodes not all nodes. each pair represents the targetable values for a quadrant
-  // css polygons have a percentage based scale where
-  // the upper left is 0% 0% and the lower right is 100% 100%, these values represent these.
-  const initialNodeValPairs = [
-    // upper left, lower left
-    [0, 50],
-    [50, 100],
-    // upper right, lower right
-    [100, 50],
-    [50, 100],
-  ];
-
-  const chooseNodeValues = (nodeValPairs: number[][]) => {
-    const targetIndex = randomIntFromInterval(0, nodeValPairs.length - 1); // will randomly select index to modify values at.
-    return targetIndex;
-  };
-  // customizable node values
-  const setNodeVals = (targetIndex: number, nodeValuePairs: number[][], probability: number = 0.3) => {
-    // set the probability that a modification will take place. currently 30%
-    const modCheckPass = Math.random() <= probability ? true : false;
-    // if check doesn't pass dont modify any node values
-    if (modCheckPass === false) return nodeValuePairs;
-    // choose values to modify by target index
-    const moddedValues = nodeValuePairs.map((valPair: number[], index) => {
-      if (index === targetIndex) {
-        // modify the values;
-        return valPair.map((value: number) => {
-          return randomIntFromInterval(0, 80);
-        });
-      } else {
-        // leave untargeted indexes alone.
-        return valPair;
-      }
-    });
-    return moddedValues;
-  };
-  // memoize all to avoid values changing in non deterministic way during rerendering due to random values.
-  const targetIndex = useMemo(() => chooseNodeValues(initialNodeValPairs), []);
-  const nodeVals = useMemo(() => setNodeVals(targetIndex, initialNodeValPairs, 0.4), []);
-  const shapeStyles = {
-    height: `${height}px`,
-    width: `${width}px`,
-    clipPath: `polygon(${nodeVals[0][0]}% 0, ${nodeVals[2][0]}% 0, 100% ${nodeVals[2][1]}%, 100% 100%, 0 100%, 0 ${nodeVals[0][1]}%)`,
-    transitionDelay: `${delay * 120}ms`,
-  };
-  return (
-    <>
-      {render && (
-        <div
-          ref={shapeRef}
-          data-effect
-          style={shapeStyles}
-          className={`structure ${
-            build && "build"
-          } flex flex-wrap place-content-evenly gap-[2px] border border-slate-600 bg-slate-600`}
-          onTransitionEnd={handleTransitionEnd}
-        >
-          <Windows shapeH={height} shapeW={width} delay={delay} />
-        </div>
-      )}
-    </>
-  );
-};
 
 const Windows = ({ shapeH, shapeW, delay }) => {
   const rows = Math.floor(shapeH / 7.5);
@@ -134,6 +51,82 @@ const Window = ({ showWindow }) => {
   return <div className={`window ${lightOn && "show"} h-[3px] w-[3px] bg-gray-100`} />;
 };
 
+// only represent values for targetable nodes not all nodes. each pair represents the targetable values for a quadrant
+// css polygons have a percentage based scale where
+// the upper left is 0% 0% and the lower right is 100% 100%, these values represent these.
+const initialNodeValPairs = [
+  // upper left, lower left
+  [0, 50],
+  [50, 100],
+  // upper right, lower right
+  [100, 50],
+  [50, 100],
+];
+
+const chooseNodeValues = (nodeValPairs: number[][]) => {
+  const targetIndex = randomIntFromInterval(0, nodeValPairs.length - 1); // will randomly select index to modify values at.
+  return targetIndex;
+};
+// customizable node values
+const setNodeVals = (targetIndex: number, nodeValuePairs: number[][], probability: number = 0.3) => {
+  // set the probability that a modification will take place. currently 30%
+  const modCheckPass = Math.random() <= probability ? true : false;
+  // if check doesn't pass dont modify any node values
+  if (modCheckPass === false) return nodeValuePairs;
+  // choose values to modify by target index
+  const moddedValues = nodeValuePairs.map((valPair: number[], index) => {
+    if (index === targetIndex) {
+      // modify the values;
+      return valPair.map((value: number) => {
+        return randomIntFromInterval(0, 80);
+      });
+    } else {
+      // leave untargeted indexes alone.
+      return valPair;
+    }
+  });
+  return moddedValues;
+};
+
+const Building = ({ delay, height, width }) => {
+  // const [build, setBuild] = useState(false);
+  const [, setTransitionEnd] = useState(false);
+  const shapeRef = useRef(null);
+
+  const handleTransitionEnd = () => {
+    setTransitionEnd(true);
+  };
+
+  // useEffect(() => {
+  //   setBuild(true);
+  // }, []);
+
+  // memoize all to avoid values changing in non deterministic way during rerendering due to random values.
+  const targetIndex = chooseNodeValues(initialNodeValPairs);
+  const nodeVals = setNodeVals(targetIndex, initialNodeValPairs, 0.4);
+  const shapeStyles = {
+    height: `${height}px`,
+    width: `${width}px`,
+    clipPath: `polygon(${nodeVals[0][0]}% 0, ${nodeVals[2][0]}% 0, 100% ${nodeVals[2][1]}%, 100% 100%, 0 100%, 0 ${nodeVals[0][1]}%)`,
+    transitionDelay: `${delay}ms`,
+  };
+  return (
+    <>
+      <div
+        ref={shapeRef}
+        data-effect
+        style={shapeStyles}
+        className={`structure flex origin-bottom flex-wrap place-content-evenly
+          gap-[2px] border border-slate-600 bg-slate-600 
+          transition-transform duration-[200ms]`}
+        onTransitionEnd={handleTransitionEnd}
+      >
+        <Windows shapeH={height} shapeW={width} delay={delay} />
+      </div>
+    </>
+  );
+};
+
 type CityChunkProps = {
   direction: "left" | "right";
   cityChunkWidth: number;
@@ -154,7 +147,9 @@ const CityChunk = ({ direction, cityChunkWidth }: CityChunkProps) => {
     while (remainingChunkSpace > 0) {
       const structureWidth: number = randomIntFromInterval(15, 25);
       const structureHeight: number = calcHeightMod(i, 20, 60);
-      structs.push(<Building key={`struct${i}`} width={structureWidth} height={structureHeight} delay={i} />);
+      structs.push(
+        <Building key={`struct${i}`} width={structureWidth} height={structureHeight} delay={i * 85} />,
+      );
       remainingChunkSpace -= structureWidth;
       i++;
     }
@@ -175,13 +170,8 @@ const Skyline = () => {
   const [delayEffectMs, setDelayEffectMs] = useState<number>(0);
   const forestWidthRef = useRef<HTMLDivElement>(null);
   const cityWidthRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const forestWidth = Math.floor(forestWidthRef.current?.getBoundingClientRect().width);
-    setForestWidth(forestWidth);
-    const cityWidth = Math.floor(cityWidthRef.current?.getBoundingClientRect().width);
-    setCityWidth(cityWidth);
-  }, [forestWidthRef, cityWidth]);
+  // necessary to get dynamic width stored in root element style attribute, since it takes a sec to load
+  const rootStyleLoaded = useRootMutationObserver();
 
   useEffect(() => {
     const handleSkylineWidthChange = () => {
@@ -189,9 +179,16 @@ const Skyline = () => {
         window.getComputedStyle(document.documentElement).getPropertyValue("--info-cont-width"),
       );
     };
-    const infoContWidthChangeEvent = new Event("widthchange");
-    console.log(skylineWidth);
-  }, [skylineWidth]);
+
+    const forestWidth = Math.floor(forestWidthRef.current?.getBoundingClientRect().width);
+    const cityWidth = Math.floor(cityWidthRef.current?.getBoundingClientRect().width);
+
+    setCityWidth(cityWidth);
+    setForestWidth(forestWidth);
+
+    const infoContainer: HTMLElement = document.querySelector("html");
+    handleSkylineWidthChange();
+  }, [skylineWidth, rootStyleLoaded]);
 
   return (
     <div
@@ -202,13 +199,13 @@ const Skyline = () => {
       <div data-forest ref={forestWidthRef} className="absolute bottom-0 left-0 flex h-full w-1/4 items-end">
         <Forest chunkWidth={forestWidth} direction={"left"} setDelayEffectMs={setDelayEffectMs} />
       </div>
-      <div ref={cityWidthRef} className="absolute flex w-1/2">
+      <div ref={cityWidthRef} className="absolute flex h-full w-1/2">
         <CityChunk direction="left" cityChunkWidth={Math.floor(cityWidth / 4)} />
         <CityPark delayEffectMs={delayEffectMs} />
         <CityChunk direction="right" cityChunkWidth={Math.floor(cityWidth / 4)} />
       </div>
       <div data-forest className="absolute -right-2 flex h-full w-1/4 items-end">
-        <Forest setDelayEffectMs={setDelayEffectMs} chunkWidth={forestWidth} direction={"right"} />
+        <Forest chunkWidth={forestWidth} direction={"right"} setDelayEffectMs={setDelayEffectMs} />
       </div>
     </div>
   );
