b7c32ac766498f1643161038138feb25bf70a6c8
Gabriel <gkime91@gmail.com>
2024-05-02 22:49:32 -0700

    chunk data now properly used to render city structures

--- src/components/bgEffects/Skyline.tsx
+++ src/components/bgEffects/Skyline.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useMemo, useEffect, useRef, type Dispatch, type SetStateAction } from "react";
 import useRootMutationObserver from "../../scripts/hooks/useMutationObserver";
+import useGetDataFromDataAttribute from "../../scripts/hooks/useGetDataFromDataAttribute";
 import { randomIntFromInterval } from "../../scripts/randomFromInterval";
 import Forest from "./Forest";
 import CityPark from "./CityPark";
@@ -57,44 +58,7 @@ const Windows = ({ shapeH, shapeW, delay }) => {
   return windows.sort((a, b) => (a < b ? 1 : -1));
 };
 
-// only represent values for targetable nodes not all nodes. each pair represents the targetable values for a quadrant
-// css polygons have a percentage based scale where
-// the upper left is 0% 0% and the lower right is 100% 100%, these values represent these.
-const initialNodeValPairs = [
-  // upper left, lower left
-  [0, 50],
-  [50, 100],
-  // upper right, lower right
-  [100, 50],
-  [50, 100],
-];
-
-const chooseNodeValues = (nodeValPairs: number[][]) => {
-  const targetIndex = randomIntFromInterval(0, nodeValPairs.length - 1); // will randomly select index to modify values at.
-  return targetIndex;
-};
-// customizable node values
-const setNodeVals = (targetIndex: number, nodeValuePairs: number[][], probability: number = 0.3) => {
-  // set the probability that a modification will take place. currently 30%
-  const modCheckPass = Math.random() <= probability ? true : false;
-  // if check doesn't pass dont modify any node values
-  if (modCheckPass === false) return nodeValuePairs;
-  // choose values to modify by target index
-  const moddedValues = nodeValuePairs.map((valPair: number[], index) => {
-    if (index === targetIndex) {
-      // modify the values;
-      return valPair.map((value: number) => {
-        return randomIntFromInterval(0, 80);
-      });
-    } else {
-      // leave untargeted indexes alone.
-      return valPair;
-    }
-  });
-  return moddedValues;
-};
-
-const Building = ({ delay, height, width }) => {
+const Building = ({ height, width, nodeVals, transitionDelay }) => {
   // const [build, setBuild] = useState(false);
   const [, setTransitionEnd] = useState(false);
   const shapeRef = useRef(null);
@@ -103,19 +67,11 @@ const Building = ({ delay, height, width }) => {
     setTransitionEnd(true);
   };
 
-  // useEffect(() => {
-  //   setBuild(true);
-  // }, []);
-
-  // memoize all to avoid values changing in non deterministic way during rerendering due to random values.
-  
-  const targetIndex = chooseNodeValues(initialNodeValPairs);
-  const nodeVals = setNodeVals(targetIndex, initialNodeValPairs, 0.4);
   const shapeStyles = {
     height: `${height}px`,
     width: `${width}px`,
     clipPath: `polygon(${nodeVals[0][0]}% 0, ${nodeVals[2][0]}% 0, 100% ${nodeVals[2][1]}%, 100% 100%, 0 100%, 0 ${nodeVals[0][1]}%)`,
-    transitionDelay: `${delay}ms`,
+    transitionDelay: `${transitionDelay}ms`,
   };
   return (
     <>
@@ -128,7 +84,7 @@ const Building = ({ delay, height, width }) => {
           transition-transform duration-[200ms]`}
         onTransitionEnd={handleTransitionEnd}
       >
-        <Windows shapeH={height} shapeW={width} delay={delay} />
+        <Windows shapeH={height} shapeW={width} delay={transitionDelay} />
       </div>
     </>
   );
@@ -136,66 +92,57 @@ const Building = ({ delay, height, width }) => {
 
 type CityChunkProps = {
   direction: "left" | "right";
-  cityChunkWidth: number;
+  chunkData: any;
 };
 
-const CityChunk = ({ direction, cityChunkWidth }: CityChunkProps) => {
-  const renderStructures = (direction: "left" | "right") => {
-    const calcHeightMod = (index: number, min: number, max: number) => {
-      const noise = randomIntFromInterval(-10, 10);
-      const height = 10 * index + 30 * Math.cos((20 * i) ** 1 / 2) + noise;
-      const heightClamp = height > max ? randomIntFromInterval(10, height / 2) : 0;
-      return height < min ? min : height - heightClamp;
-    };
+const CityChunk = ({ direction, chunkData }: CityChunkProps) => {
+  const sortedChunkData =
+    direction === "left"
+      ? chunkData.sort((a, b) => (a.transitionDelay < b.transitionDelay ? 1 : -1))
+      : chunkData.sort((a, b) => (a.transitionDelay < b.transitionDelay ? -1 : 1));
 
-    const structs = [];
-    let remainingChunkSpace = cityChunkWidth;
-    let i = 0;
-    while (remainingChunkSpace > 0) {
-      const structureWidth: number = randomIntFromInterval(15, 25);
-      const structureHeight: number = calcHeightMod(i, 20, 60);
-      structs.push(
-        <Building key={`struct${i}`} width={structureWidth} height={structureHeight} delay={i * 85} />,
-      );
-      remainingChunkSpace -= structureWidth;
-      i++;
-    }
-    return direction === "left"
-      ? structs.sort((a, b) => (a.props.index < b.props.index ? 1 : -1))
-      : structs.sort((a, b) => (a.props.index < b.props.index ? -1 : 1));
-  };
+  const cityChunk = sortedChunkData.map((obj, i) => {
+    const { nodeVals, width, height, transitionDelay } = obj;
+    return (
+      <Building
+        key={`struct${i}`}
+        nodeVals={nodeVals}
+        width={width}
+        height={height}
+        transitionDelay={transitionDelay}
+      />
+    );
+  });
 
-  const chunk = renderStructures(direction);
+  return <div className={`bottom-1 flex w-1/3 items-end justify-end gap-2`}>{cityChunk}</div>;
+};
 
-  return <div className={`bottom-1 flex w-1/3 items-end justify-end gap-2`}>{chunk}</div>;
+type buildingData = {
+  nodeVals: number[];
+  height: number;
+  width: number;
+  transitionDelay: number;
+};
+type cityData = {
+  skylineLeft: buildingData[];
+  skylineRight: buildingData[];
 };
 
 const Skyline = () => {
+  const [renderSkyline, setRenderSkyline] = useState(true);
   const [forestWidth, setForestWidth] = useState<number>(0);
   const [skylineWidth, setSkylineWidth] = useState("");
   const [cityWidth, setCityWidth] = useState<number>(0);
+  const [cityData, setCityData] = useState<cityData>({ skylineLeft: [], skylineRight: [] });
   const [delayEffectMs, setDelayEffectMs] = useState<number>(0);
   const forestWidthRef = useRef<HTMLDivElement>(null);
   const cityWidthRef = useRef<HTMLDivElement>(null);
-  // necessary to get dynamic width stored in root element style attribute, since it takes a sec to load
-  const rootStyleLoaded = useRootMutationObserver();
 
+  const cityChunks = useGetDataFromDataAttribute("data-skyline-data");
+  console.log(cityChunks);
   useEffect(() => {
-    const handleSkylineWidthChange = () => {
-      setSkylineWidth(
-        window.getComputedStyle(document.documentElement).getPropertyValue("--info-cont-width"),
-      );
-    };
-
-    const forestWidth = Math.floor(forestWidthRef.current?.getBoundingClientRect().width);
-    const cityWidth = Math.floor(cityWidthRef.current?.getBoundingClientRect().width);
-
-    setCityWidth(cityWidth);
-    setForestWidth(forestWidth);
-
-    const infoContainer: HTMLElement = document.querySelector("html");
-    handleSkylineWidthChange();
-  }, [skylineWidth, rootStyleLoaded]);
+    setCityData(cityChunks);
+  }, [cityChunks]);
 
   return (
     <div
@@ -207,9 +154,9 @@ const Skyline = () => {
         <Forest chunkWidth={forestWidth} direction={"left"} setDelayEffectMs={setDelayEffectMs} />
       </div>
       <div ref={cityWidthRef} className="absolute flex h-full w-1/2">
-        <CityChunk direction="left" cityChunkWidth={Math.floor(cityWidth / 4)} />
+        <CityChunk direction="left" chunkData={cityData.skylineLeft} />
         <CityPark delayEffectMs={delayEffectMs} />
-        <CityChunk direction="right" cityChunkWidth={Math.floor(cityWidth / 4)} />
+        <CityChunk direction="right" chunkData={cityData.skylineRight} />
       </div>
       <div data-forest className="absolute -right-2 flex h-full w-1/4 items-end">
         <Forest chunkWidth={forestWidth} direction={"right"} setDelayEffectMs={setDelayEffectMs} />
