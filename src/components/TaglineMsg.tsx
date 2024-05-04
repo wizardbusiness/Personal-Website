a819a9abaf32ebaf26b672bb2fd9e27737887175
Gabriel <gkime91@gmail.com>
2024-05-03 17:40:29 -0700

    added in missing media query heights, and changed breakpoint from lg to xl for height

--- src/components/TaglineMsg.tsx
+++ src/components/TaglineMsg.tsx
@@ -16,7 +16,7 @@ const TaglineMsg = ({ msg, blinkCursor, scaleText }) => {
   }, [msgIndex]);
 
   return (
-    <div id="caption" className="flex items-center gap-2 [transform-origin:center]">
+    <div id="caption" className="flex h-[12vh] items-center gap-2 [transform-origin:center] xl:h-[14vh]">
       <h1 className="no-wrap h-fit text-center font-mono text-xl text-gray-200 lg:text-3xl xl:text-5xl 2xl:text-7xl">
         {(msgIndex === msg.length &&
           text.split(" ").map((str, index) => {
