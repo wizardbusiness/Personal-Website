47dbe0c2f469d3af8dfacdca2c4db019e238866d
Gabriel <gkime91@gmail.com>
2024-05-07 14:14:48 -0700

    removed unused imports and state

--- src/components/bgEffects/Forest.tsx
+++ src/components/bgEffects/Forest.tsx
@@ -1,18 +1,11 @@
-import React, { useEffect, useState } from "react";
-import {
-  randomIntFromInterval,
-  randomDecNumFromInterval,
-  randomElFromArray,
-} from "../../scripts/randomFromInterval";
+import React from "react";
+
 import FirTree from "./treeSvgComponents/FirTree";
 
-const Forest = ({ forestData, direction, renderSkyline }) => {
-  // const [forestChunk, setForestChunk] = useState([]);
-
+const Forest = ({ forestData, renderSkyline }) => {
   const forestChunk = forestData.map((treeProps, index) => {
     return <FirTree key={`tree${index}`} {...treeProps} renderSkyline={renderSkyline} />;
   });
-
   return <>{forestChunk}</>;
 };
 
