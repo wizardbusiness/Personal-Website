71b1a3589ca81b4f636933700b089187bba020e7
Gabriel <gkime91@gmail.com>
2024-05-06 20:46:12 -0700

    fixed slight desync between cc and star trails on transition to info section

--- src/scripts/transition.ts
+++ src/scripts/transition.ts
@@ -494,7 +494,7 @@ function goToInfoSection() {
           callback: function (...args: [TransitionStep]) {
             setTimeout(() => {
               setCurrTransitionStep(...args);
-            }, 1000);
+            }, 1800);
           },
         },
         {
